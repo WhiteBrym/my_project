5c9511c6f5a472a1b3f129232a1324beb55d185e
WhiteBrym <lobachyrui@gmail.com>
2019-12-01 21:42:03 +0300

    Updates

---
+++ precache-manifest.7a58506226ded91f568b199a7b962c86.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d849dd42c36871eae9ec6753d11c2c",
+    "url": "/my_project/index.html"
+  },
+  {
+    "revision": "53daf244a39679fa958e",
+    "url": "/my_project/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b00d91f291fec5d11faf",
+    "url": "/my_project/static/css/main.e6f51c76.chunk.css"
+  },
+  {
+    "revision": "53daf244a39679fa958e",
+    "url": "/my_project/static/js/2.e09be37f.chunk.js"
+  },
+  {
+    "revision": "b00d91f291fec5d11faf",
+    "url": "/my_project/static/js/main.36bfa0c2.chunk.js"
+  },
+  {
+    "revision": "d917391371477606f09e",
+    "url": "/my_project/static/js/runtime-main.3d8f6ed8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my_project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/my_project/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my_project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my_project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my_project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
